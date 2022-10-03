53f55eca2bd4290a3f57cd22c857fab62e359164
DeMineArchiver <83916788+DeMineArchiver@users.noreply.github.com>
2022-10-03 18:27:18 +0300

    Remove BRUH

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     if(introEnd) {
         if(window.scrollY > 0) {
-            console.log("BRUH")
             header.classList.add("top");
         } else {
             header.classList.remove("top");
